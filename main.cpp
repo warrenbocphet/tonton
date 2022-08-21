4c1639aeb8eb5031d5e0501dee8c888e590d502f
vitsensei <anh.tran0248@gmail.com>
2022-08-21 14:57:51 +1000

    refactor(main): make main.cpp more modern
    Not sure why Slamtec decide to not use smart pointer to make their life easier.
    
    Also add some code to capture data.
    
    Also a change in clang-format

--- main.cpp
+++ main.cpp
@@ -1,36 +1,65 @@
 #include <iostream>
+#include <fstream>
+#include <memory.h>
+#include <thread>
 
 #include "sl_lidar.h"
 #include "sl_lidar_driver.h"
+#include "rplidar.h"
+
+#include <fmt/format.h>
 
 int main(int argc, char** argv)
 {
     //  Create a communication channel instance
-    sl::IChannel*             _channel;
-    sl::Result<sl::IChannel*> channel = sl::createSerialPortChannel("/dev/ttyUSB0", 115200);
+    std::shared_ptr<sl::IChannel> channel(*sl::createSerialPortChannel("/dev/ttyUSB0", 115200));
 
     ///  Create a LIDAR driver instance
-    sl::ILidarDriver* lidar = *sl::createLidarDriver();
-    auto              res   = lidar->connect(*channel);
+    std::shared_ptr<sl::ILidarDriver> lidar(*sl::createLidarDriver());
 
-    if (SL_IS_OK(res))
+    if (SL_IS_OK(lidar->connect(channel.get())))
     {
         sl_lidar_response_device_info_t deviceInfo;
-        res = lidar->getDeviceInfo(deviceInfo);
-        if (SL_IS_OK(res))
+        if (SL_IS_OK(lidar->getDeviceInfo(deviceInfo)))
         {
-            printf(
-                "Model: %d, Firmware Version: %d.%d, Hardware Version: %d\n",
-                deviceInfo.model,
-                deviceInfo.firmware_version >> 8,
-                deviceInfo.firmware_version & 0xffu,
-                deviceInfo.hardware_version
-            );
+            std::cout << fmt::format(
+                             "Model: {}\nFirmware version: {}.{}\nHardware version: {}",
+                             deviceInfo.model,
+                             deviceInfo.firmware_version >> 8,
+                             deviceInfo.firmware_version & 0xffu,
+                             deviceInfo.hardware_version
+                         )
+                      << std::endl;
         }
-        else { fprintf(stderr, "Failed to get device information from LIDAR %08x\r\n", res); }
+        else
+        {
+            throw std::runtime_error("Failed to get information from the lidar.");
+        }
     }
-    else { fprintf(stderr, "Failed to connect to LIDAR %08x\r\n", res); }
+    else
+    {
+        throw std::runtime_error("Failed to connect to lidar.");
+    }
 
-    delete lidar;
-    delete _channel;
+    sl::LidarScanMode scan_mode;
+    if (SL_IS_OK(lidar->startScan(false, true, 0, &scan_mode)))
+    {
+        std::cout << "Using scan mode: " << scan_mode.scan_mode << std::endl;
+    }
+    else
+    {
+        std::cout << "Failed to start a scan." << std::endl;
+    }
+
+    sl_lidar_response_measurement_node_hq_t nodes[8192];
+    size_t                                  nodeCount = sizeof(nodes) / sizeof(sl_lidar_response_measurement_node_hq_t);
+
+    if (SL_IS_OK(lidar->grabScanDataHq(nodes, nodeCount)))
+    {
+        std::cout << fmt::format("Grabbed {} lidar data.", nodeCount) << std::endl;
+    }
+    else
+    {
+        std::cout << "Failed to grab the scan data" << std::endl;
+    }
 }
